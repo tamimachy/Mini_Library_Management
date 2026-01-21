1501501007b41d7e7897d0dcf715e068f49e5702
USER <tamimachy9897@gmail.com>
2026-01-21 23:45:13 +0600

    DTOs and Controller Added.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mini_Library_Management.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
